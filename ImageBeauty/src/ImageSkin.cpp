7c79055121160827d6c7356a1c1f649d11be6a89
DavidSunny86 <sunjun8601@126.com>
2016-07-03 22:50:40 +0800

    【Reasons】 add Files 【author】 David.Sunny

--- ImageBeauty/src/ImageSkin.cpp
+++ ImageBeauty/src/ImageSkin.cpp
@@ -8,15 +8,41 @@ ImageSkin::ImageSkin()
 
 cv::Mat ImageSkin::skinWhiteing(Mat& srcMat)
 {
+	if (srcMat.empty())
+	{
+		return Mat();
+	}
 	Mat outDst;
-	GaussianBlur(srcMat, outDst, Size(3,3),0,0);
-
-	bilateralFilter(outDst, outDst, 9, 18, 4.5);
-
-	Mat kernel(Size(3, 3), CV_32F,Scalar(-1));
-	kernel.at<float>(1, 1) = 5;
 	Mat result;
-	cv::filter2D(outDst, result, outDst.depth(), kernel);
+	GaussianBlur(srcMat, outDst, Size(3,3),0,0);
+	bilateralFilter(outDst, result, 5, 10, 2.5);
+	Mat kernel(Size(3, 3), CV_32F,Scalar(0));
+	kernel.at<float>(1, 1) = 5;
+	kernel.at<float>(0, 1) = -1;
+	kernel.at<float>(1, 2) = -1;
+	kernel.at<float>(1, 0) = -1;
+	kernel.at<float>(2, 1) = -1;
+	cv::filter2D(result, result, result.depth(), kernel);
+	return result;
+}
+
+cv::Mat skinWhiteing(Mat& srcMat,int bilateralSize,int sharpenSize)
+{
+	if (srcMat.empty())
+	{
+		return Mat();
+	}
+	Mat outDst;
+	Mat result;
+	GaussianBlur(srcMat, outDst, Size(3,3),0,0);
+	bilateralFilter(outDst, result, bilateralSize, 2*bilateralSize, 0.5*bilateralSize);
+	Mat kernel(Size(3, 3), CV_32F,Scalar(0));
+	kernel.at<float>(1, 1) = sharpenSize;
+	kernel.at<float>(0, 1) = -1;
+	kernel.at<float>(1, 2) = -1;
+	kernel.at<float>(1, 0) = -1;
+	kernel.at<float>(2, 1) = -1;
+	cv::filter2D(result, result, result.depth(), kernel);
 	return result;
 }
 
--- ImageBeauty/src/ImageSkin.h
+++ ImageBeauty/src/ImageSkin.h
@@ -1,12 +1,12 @@
 #pragma once
-#include "ImageBeauityUtils.h"
+#include "ImageBeautyUtils.h"
 
 class IMAGEBEAUITY_MODEL_EXPORT ImageSkin
 {
 public:
 	ImageSkin();
 	Mat skinWhiteing(Mat& srcMat);
-	
+	Mat skinWhiteing(Mat& srcMat,int bilateralSize,int sharpenSize);
 	~ImageSkin();
 };
 
