d4dafb5ad0eb9db13b8d860908c326edc57f34a1
david.sun <david.sun@hzleaper.com>
2016-07-04 10:40:08 +0800

    [Reasons] modify [author] David.Sun

--- ImagePro/src/ImageFFT.h
+++ ImagePro/src/ImageFFT.h
@@ -1,10 +1,20 @@
 #pragma once
 #include "ImageProCom.h"
 
+
+#define GRAY_THRESH 150
+#define HOUGH_VOTE 100
+
 class IMAGEPRO_MODEL_EXPORT ImageFFT
 {
 public:
 	ImageFFT();
 	Mat getFftImage(Mat& srcMat);
+	Mat imageFftPre(Mat& srcMat);
+
+
+private:
+	Mat ImageFft_ocrCorrectingSample(Mat& srcMat);
+
 	~ImageFFT();
 };
